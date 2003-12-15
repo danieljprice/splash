0835dfc0e3484a19636eaa7deb5406919d6949dd
dprice <dprice>
2003-12-15 12:47:03 +0000

    lower case

--- modules.f
+++ modules.f
@@ -6,120 +6,120 @@
 !
 !--global parameters  (should really allocate memory appropriately)
 !
-      MODULE params
-       IMPLICIT NONE
-       INTEGER, PARAMETER :: max=55000
-       INTEGER, PARAMETER :: maxstep=100
-       INTEGER, PARAMETER :: ndimmax = 3
-       INTEGER, PARAMETER :: maxplot=24+2*ndimmax + 1	! maximum number of plots       
-      END MODULE params
+      module params
+       implicit none
+       integer, parameter :: max=55000
+       integer, parameter :: maxstep=100
+       integer, parameter :: ndimmax = 3
+       integer, parameter :: maxplot=24+2*ndimmax + 1	! maximum number of plots       
+      end module params
 !
 !--particle data
 !      
-      MODULE particle_data
-       USE params
-       IMPLICIT NONE
-       INTEGER, DIMENSION(maxstep) :: npart,ntot,nghost,ntotplot
-       INTEGER, DIMENSION(max) :: iam
-       REAL, DIMENSION(maxstep) :: time, gamma
-       REAL, DIMENSION(maxplot,max,maxstep) :: dat
-       REAL, DIMENSION(maxplot,2) :: lim
-       REAL :: hfact       
-       REAL :: Bmin,Bmax
+      module particle_data
+       use params
+       implicit none
+       integer, dimension(maxstep) :: npart,ntot,nghost,ntotplot
+       integer, dimension(max) :: iam
+       real, dimension(maxstep) :: time, gamma
+       real, dimension(maxplot,max,maxstep) :: dat
+       real, dimension(maxplot,2) :: lim
+       real :: hfact       
+       real :: bmin,bmax
        
-      END MODULE particle_data
+      end module particle_data
 !
 !--filename
 !      
-      MODULE filenames
-       IMPLICIT NONE
-       INTEGER :: ifile,nfilesteps
-       CHARACTER(LEN=20) :: rootname
-      END MODULE filenames
+      module filenames
+       implicit none
+       integer :: ifile,nfilesteps
+       character(len=20) :: rootname
+      end module filenames
 !
 !--labels for all plots and the locations of certain useful variables
 !
-      MODULE labels
-       USE params
-       IMPLICIT NONE
-       CHARACTER(LEN=20), DIMENSION(maxplot+2) :: label
-       CHARACTER(LEN=1), DIMENSION(3) :: labelcoord
-       INTEGER, DIMENSION(3) :: ix
-       INTEGER :: ivx,ivlast,irho,iutherm,ipr,ih,irad,iBfirst,iBlast
-       INTEGER :: ipmass
-       INTEGER :: ientrop,irad2,ipmag,ibeta,itotpr,ike,idivB,idivBerr
-       INTEGER :: iACplane,itimestep,ipowerspec
-      END MODULE labels
+      module labels
+       use params
+       implicit none
+       character(len=20), dimension(maxplot+2) :: label
+       character(len=1), dimension(3) :: labelcoord
+       integer, dimension(3) :: ix
+       integer :: ivx,ivlast,irho,iutherm,ipr,ih,irad,ibfirst,iblast
+       integer :: ipmass
+       integer :: ientrop,irad2,ipmag,ibeta,itotpr,ike,idivb,idivberr
+       integer :: iacplane,itimestep,ipowerspec
+      end module labels
 !
 !--module containing plot settings
 !
-      MODULE settings
-       USE params
-       IMPLICIT NONE
-       INTEGER :: numplot,ncalc,ncolumns,nextra,nacross,ndown
-       INTEGER :: ndataplots
-       INTEGER :: ndim, ndimV       
-       INTEGER :: imark, imarkg, imarksink
-       INTEGER :: ixsec,nxsec, nbins,nc,icircpart
-       INTEGER :: linestylein, iexact
-       INTEGER :: irenderplot
-       INTEGER :: ncolours,nstart,n_end,nfreq
+      module settings
+       use params
+       implicit none
+       integer :: numplot,ncalc,ncolumns,nextra,nacross,ndown
+       integer :: ndataplots
+       integer :: ndim, ndimv       
+       integer :: imark, imarkg, imarksink
+       integer :: ixsec,nxsec, nbins,nc,icircpart
+       integer :: linestylein, iexact
+       integer :: irenderplot
+       integer :: ncolours,nstart,n_end,nfreq
        
-       INTEGER :: ncontours_nomulti,npix_nomulti,npixvec_nomulti
-       INTEGER :: ivecplot_nomulti,irender,icolours
-       INTEGER :: ipapersize,menuitems
+       integer :: ncontours_nomulti,npix_nomulti,npixvec_nomulti
+       integer :: ivecplot_nomulti,irender,icolours
+       integer :: ipapersize,menuitems
        
-       REAL :: scalemax,zoom
-       REAL :: xsecpos_nomulti
-       REAL :: papersizex,aspectratio
+       real :: scalemax,zoom
+       real :: xsecpos_nomulti
+       real :: papersizex,aspectratio
 !--plot options
-       LOGICAL :: animate,iadapt,magfield,iexist,ihavereadfilename
-       LOGICAL :: plotcirc,plotcircall,flythru,imulti,ipagechange
-       LOGICAL :: iplotline,iplotlinein,iplotav,ilabelpart
-       LOGICAL :: iplotpart,iplotghost,iplotsink
-       LOGICAL :: ishowopts, ivegotdata,isamexaxis
+       logical :: animate,iadapt,magfield,iexist,ihavereadfilename
+       logical :: plotcirc,plotcircall,flythru,imulti,ipagechange
+       logical :: iplotline,iplotlinein,iplotav,ilabelpart
+       logical :: iplotpart,iplotghost,iplotsink
+       logical :: ishowopts, ivegotdata,isamexaxis
        
-       LOGICAL :: backgnd_vec_nomulti
-       LOGICAL :: iplotcont_nomulti,xsec_nomulti,iplotpartvec_nomulti
+       logical :: backgnd_vec_nomulti
+       logical :: iplotcont_nomulti,xsec_nomulti,iplotpartvec_nomulti
 
-      END MODULE settings
+      end module settings
 !
 !--multiplot settings
 !      
-      MODULE multiplot
-       USE params
-       IMPLICIT NONE
-       INTEGER, DIMENSION(maxplot) :: multiplotx,multiploty
-       INTEGER, DIMENSION(maxplot) :: irendermulti,ivecplotmulti      
-       INTEGER, DIMENSION(maxplot) :: npixmulti,npixvecmulti 
-       INTEGER, DIMENSION(maxplot) :: ncontoursmulti,itrans
-       INTEGER :: nyplotmulti
-       LOGICAL, DIMENSION(maxplot) :: iplotcontmulti, iplotpartvecmulti     
-       LOGICAL, DIMENSION(maxplot) :: x_secmulti,backgnd_vec_multi
-       REAL, DIMENSION(maxplot) :: xsecposmulti
-      END MODULE multiplot
+      module multiplot
+       use params
+       implicit none
+       integer, dimension(maxplot) :: multiplotx,multiploty
+       integer, dimension(maxplot) :: irendermulti,ivecplotmulti      
+       integer, dimension(maxplot) :: npixmulti,npixvecmulti 
+       integer, dimension(maxplot) :: ncontoursmulti,itrans
+       integer :: nyplotmulti
+       logical, dimension(maxplot) :: iplotcontmulti, iplotpartvecmulti     
+       logical, dimension(maxplot) :: x_secmulti,backgnd_vec_multi
+       real, dimension(maxplot) :: xsecposmulti
+      end module multiplot
 !      
 !--exact solution parameters      
 !
-      MODULE exact_params
-       IMPLICIT NONE
-       INTEGER, PARAMETER :: ipolycmax=1000
+      module exact_params
+       implicit none
+       integer, parameter :: ipolycmax=1000
 !--toy star
-       INTEGER :: norder
-       REAL :: Htstar,Atstar,Ctstar,totmass,sigma,sigma0 ! toy star parameters
+       integer :: norder
+       real :: htstar,atstar,ctstar,totmass,sigma,sigma0 ! toy star parameters
 !--sound wave      
-       REAL delta,lambda
+       real delta,lambda
 !--polytrope
-       INTEGER :: ipolyc
-       REAL mtot,maxrho,akfac,den(ipolycmax),rad(ipolycmax)       
-      END MODULE exact_params
+       integer :: ipolyc
+       real mtot,maxrho,akfac,den(ipolycmax),rad(ipolycmax)       
+      end module exact_params
 !
 !--tabulated column density through the kernel 
-!  (used in interpolate3D_projection)
+!  (used in interpolate3d_projection)
 !
-      MODULE column
-       IMPLICIT NONE
-       INTEGER, PARAMETER :: maxcoltable = 1000
-       REAL, PARAMETER :: dmaxcoltable = 1./FLOAT(maxcoltable)
-       REAL, DIMENSION(maxcoltable) :: coltable
-      END MODULE column
+      module column
+       implicit none
+       integer, parameter :: maxcoltable = 1000
+       real, parameter :: dmaxcoltable = 1./float(maxcoltable)
+       real, dimension(maxcoltable) :: coltable
+      end module column
