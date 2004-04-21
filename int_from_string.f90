45fd2680355a9783eb234fd99f266cc2de83d6b7
dprice <dprice>
2004-04-21 11:20:30 +0000

    izero : more robust

--- int_from_string.f90
+++ int_from_string.f90
@@ -7,19 +7,20 @@
 
 FUNCTION int_from_string(string)
  IMPLICIT NONE
- INTEGER :: int_from_string,idigit,i,ipower,maxdigits
+ INTEGER :: int_from_string,idigit,i,izero,ipower,maxdigits
  CHARACTER(LEN=*) :: string
  
  ipower = -1
  maxdigits = LEN(string)
  int_from_string = 0
+ izero = IACHAR('0')    ! position of zero in ASCII character set
 
  DO i=maxdigits,1,-1	! down through the characters
     IF (string(i:i).NE.' ') THEN
        ipower = ipower + 1
-       idigit = IACHAR(string(i:i)) - 48
+       idigit = IACHAR(string(i:i)) - izero
        IF ((idigit.LT.0).OR.(idigit.GT.10)) THEN
-          PRINT*,'int_from_string: non-numeric character in input string'
+          PRINT*, 'int_from_string: non-numeric character in input string'
 	  int_from_string = 0
 	  RETURN
        ELSE
