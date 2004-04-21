84320cf4dc416d9a9f86943d450a57157010f0e1
dprice <dprice>
2004-04-21 09:38:22 +0000

    fiddling

--- legend.f
+++ legend.f
@@ -8,16 +8,17 @@
       REAL hpos,vpos,t
       CHARACTER string*15
       
-      hpos=0.1	! either 0.1 or 0.75 for 1D shocks
+      hpos=0.5	! either 0.1 or 0.75 for 1D shocks
 !      vspace=1.2
-      vpos=-2.5	    
-      ndecimal = 2	! number of decimal places to display
+      vpos=0.5  !-2.5	    
+      ndecimal = 0	! number of decimal places to display
       ndec = 10**ndecimal
       IF (t.eq.0.0) t=1E-6
       MM=nint(t*ndec)
       PP=nint(log10(t)-log10(t*ndec))
       CALL PGNUMB(MM,PP,1,STRING,NC)
-      CALL PGMTEXT('T',vpos,hpos,0.0,'t='//STRING(1:NC))
+      CALL PGMTEXT('T',vpos,hpos,0.5,'t='//STRING(1:NC))
+      !!CALL PGMTEXT('T',vpos,hpos,0.5,STRING(1:NC)//' Dynamical times')
 
       RETURN
       END
