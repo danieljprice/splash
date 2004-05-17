d3cda6db74157f920b4ad151bd40d46578685434
dprice <dprice>
2004-05-17 14:19:03 +0000

    various crap

--- legend.f
+++ legend.f
@@ -5,20 +5,27 @@
       SUBROUTINE legend(t)
       IMPLICIT NONE    
       INTEGER MM,PP,NC,ndecimal,ndec
-      REAL hpos,vpos,t
+      REAL hpos,vpos,t,tplot
       CHARACTER string*15
       
-      hpos=0.5	! either 0.1 or 0.75 for 1D shocks
+      !hpos=0.5	! either 0.1 or 0.75 for 1D shocks
+      hpos=0.85
 !      vspace=1.2
-      vpos=0.5  !-2.5	    
-      ndecimal = 0	! number of decimal places to display
+      !vpos=0.5	    
+      vpos=-2.0
+      ndecimal = 2	! number of decimal places to display
       ndec = 10**ndecimal
-      IF (t.eq.0.0) t=1E-6
-      MM=nint(t*ndec)
-      PP=nint(log10(t)-log10(t*ndec))
+      IF (t.eq.0.0) THEN
+         tplot = 1E-6
+      ELSE
+         tplot = t    !/(2.*3.1415926536)
+      ENDIF
+      MM=nint(tplot*ndec)
+      PP=nint(log10(tplot)-log10(tplot*ndec))
       CALL PGNUMB(MM,PP,1,STRING,NC)
       CALL PGMTEXT('T',vpos,hpos,0.5,'t='//STRING(1:NC))
-      !!CALL PGMTEXT('T',vpos,hpos,0.5,STRING(1:NC)//' Dynamical times')
+   !!   CALL PGMTEXT('T',vpos,hpos,0.5,
+   !!  &             STRING(1:NC)//' Rotational periods')
 
       RETURN
       END
