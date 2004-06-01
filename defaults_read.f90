a6cc084799f25b0db3dc7038dec47cf34a1c46bc
dprice <dprice>
2004-06-01 09:55:31 +0000

    better error catching

--- defaults_read.f90
+++ defaults_read.f90
@@ -1,6 +1,8 @@
-!
+!-----------------------------------------------
 ! reads default options from file
-!
+! uses namelist input to group the options
+! these are specified in the modules
+!-----------------------------------------------
 subroutine defaults_read
  use multiplot
  use settings
@@ -11,22 +13,25 @@ subroutine defaults_read
  inquire (exist=iexist, file='defaults')
  if (iexist) then
     open(unit=1,file='defaults',status='old',form='formatted')
-    read(1,NML=plotopts,end=7,err=8)
-    read(1,NML=exactparams,end=7,err=8)
-    read(1,NML=multi,end=7,err=8)
+    read(1,NML=plotopts,end=77,err=8)
+    goto 9
+8   print*,'error reading plot options from defaults'
+9   continue
+    read(1,NML=exactparams,end=77,err=10)
+    goto 11
+10  print*,'error reading exact solution parameters from defaults'    
+11  continue    
+    read(1,NML=multi,end=77,err=10)
+    goto 13
+12  print*,'error reading multiplot options from defaults'
+13  continue
  close(unit=1)
  print*,'read default options from file '
-
+ return
  endif
- goto 9
-7 continue
+77 continue
  print*,'**** warning: end of file in defaults ****'
  close(unit=1)
- goto 9
-8 continue
- print*,'error reading defaults from file'
- close(unit=1)
-9 continue
 
  return
 end subroutine defaults_read
