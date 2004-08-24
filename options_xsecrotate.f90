c8483a7ca342d94ed14b79e73a79ddc17fd7b551
dprice <dprice>
2004-08-24 13:14:55 +0000

    menu cleaned up a bit

--- options_xsecrotate.f90
+++ options_xsecrotate.f90
@@ -15,11 +15,10 @@ subroutine options_xsecrotate
  print 10,xsec_nomulti,xsecpos_nomulti,irotate
 10  format(' 0) exit ',/, 		&
            ' 1) toggle cross section/projection           (',L1,' )',/, &
-           ' 2) manually set cross section position       (',f5.2,' )',/, &
-	   ' 3) interactively set cross section position  ( 2D only )',/, &
-	   ' 4) toggle rotation                           (',L1,' )',/, &
-	   ' 5) change rotation options')
- call prompt('enter option',ians,0,5)
+           ' 2) set cross section position                (',f5.2,' )',/, &
+	   ' 3) toggle rotation                           (',L1,' )',/, &
+	   ' 4) change rotation options')
+ call prompt('enter option',ians,0,4)
 !
 !--options
 !
@@ -38,47 +37,47 @@ subroutine options_xsecrotate
                xsecpos_nomulti)
        endif
     elseif (ndim.eq.2) then
+       call prompt('set cross section position interactively?',interact)
+       
+       if (interact) then
        !
-       !--if not already set (ie. if all = 0.0)
-       !  then set default line to diagonal across the domain
+       !--set cross section position interactively
        !
-       if (abs(xseclineX2-xseclineX1).lt.1.e-5 .and. &
-	   abs(xseclineY2-xseclineY1).lt.1.e-5) then
-	  xseclineX1 = lim(1,1)
-	  xseclineX2 = lim(1,2)
-	  xseclineY1 = lim(2,1)
-	  xseclineY2 = lim(2,2)
+          call pgbegin(0,'/xw',1,1)
+          call pgenv(lim(1,1),lim(1,2),lim(2,1),lim(2,2),1,0)
+          call pgcurs(xseclineX1,xseclineY1,char)
+          print*,'please select cross section line'
+          call pgband(1,1,xseclineX1,xseclineY1,xseclineX2,xseclineY2,char)
+          print*,'cross section line: xmin = ',xseclineX1,' xmax = ',xseclineX2
+          print*,'                    ymin = ',xseclineY1,' ymax = ',xseclineY2
+          call pgend       
+       else
+       !
+       !--set position manually
+       !
+          if (abs(xseclineX2-xseclineX1).lt.1.e-5 .and. &
+	      abs(xseclineY2-xseclineY1).lt.1.e-5) then
+          !--if not already set (ie. if all = 0.0)
+          !  then set default line to diagonal across the domain
+	     xseclineX1 = lim(1,1)
+	     xseclineX2 = lim(1,2)
+	     xseclineY1 = lim(2,1)
+	     xseclineY2 = lim(2,2)
+          endif
+          print*,'please set position of cross section through 2D data:'
+          call prompt('enter xmin of cross section line',xseclineX1)
+          call prompt('enter xmax of cross section line',xseclineX2)
+          call prompt('enter ymin of cross section line',xseclineY1)
+          call prompt('enter ymax of cross section line',xseclineY2)
        endif
-       print*,'please set position of cross section through 2D data:'
-       call prompt('enter xmin of cross section line',xseclineX1)
-       call prompt('enter xmax of cross section line',xseclineX2)
-       call prompt('enter ymin of cross section line',xseclineY1)
-       call prompt('enter ymax of cross section line',xseclineY2)
     else
        print*,'WARNING: this option has no effect in 1D'
     endif
 !------------------------------------------------------------------------
- case(3)
-    !
-    !--set cross section position interactively
-    !
-    if (ndim.eq.2) then
-       call pgbegin(0,'/xw',1,1)
-       call pgenv(lim(1,1),lim(1,2),lim(2,1),lim(2,2),1,0)
-       call pgcurs(xseclineX1,xseclineY1,char)
-       print*,'please select cross section line'
-       call pgband(1,1,xseclineX1,xseclineY1,xseclineX2,xseclineY2,char)
-       print*,'cross section line: xmin = ',xseclineX1,' xmax = ',xseclineX2
-       print*,'                    ymin = ',xseclineY1,' ymax = ',xseclineY2
-       call pgend
-    else
-       print*,'ERROR: this option applies to 2D only'
-    endif
-!------------------------------------------------------------------------
- case(4,5)
-    if (ians.eq.4) irotate = .not.irotate
+ case(3,4)
+    if (ians.eq.3) irotate = .not.irotate
     print*,'rotate = ',irotate
-    if (irotate .or. ians.eq.5) then
+    if (irotate .or. ians.eq.4) then
        call prompt('enter rotation angle about z axis in degrees',anglerot,0.,360.)
        if (ndim.eq.3) call prompt('enter tilt angle ',angletilt,-90.,90.)
        !xorigin(1:ndim) = 0.5*(lim(1:ndim,1) + lim(1:ndim,2))
