3e733c6e084797947f18112cd202643e30d4f2d6
dprice <dprice>
2004-10-11 15:50:16 +0000

    tabs removed, reads sinks (but must reorder data to plot them)

--- read_data_mbate.f90
+++ read_data_mbate.f90
@@ -38,22 +38,28 @@ subroutine read_data(rootname,istart,ifinish)
   implicit none
   integer, intent(IN) :: istart
   integer, intent(OUT) :: ifinish
-  character(LEN=3) :: fileno
   character(LEN=*), intent(IN) :: rootname
+  integer, parameter :: maxptmass = 100
   integer :: i,j,k,ifile
   integer :: ncol_max,nstep_max
   integer :: ntotin
   logical :: iexist
   real :: gammain,timeff
     
+  character(LEN=3) :: fileno
   character(LEN=LEN(rootname)+10), dimension(1000) :: filename,sinkfile
   character(LEN=LEN(rootname)+10) :: dumpfile
   integer :: int_from_string
-  integer :: nprint, nghosti, n1, n2, rhozero, RK2
-  logical :: magfield
-  real*8, dimension(:,:), allocatable :: dattemp
-  real*8 :: udisti,umassi,utimei, umagfdi, timei, gammai
-  real*8 :: escap,tkin,tgrav,tterm,tmag
+  integer :: nprint, nghosti, n1, n2, rhozero, RK2, nptmass
+  integer, dimension(:), allocatable :: isteps, iphase
+  integer, dimension(maxptmass) :: listpm
+  
+  logical :: magfield  
+  real(kind=8), dimension(:,:), allocatable :: dattemp
+  real(kind=8), dimension(:), allocatable :: dummy
+  real(kind=8) :: udisti,umassi,utimei, umagfdi, timei, gammai
+  real(kind=8) :: escap,tkin,tgrav,tterm,tmag
+  real(kind=8) :: dtmax
 
   !
   !--for rootnames without the '00', read all files starting at #1
@@ -63,7 +69,7 @@ subroutine read_data(rootname,istart,ifinish)
      if (len_trim(rootname).eq.4) then
         write(fileno,"(i1,i1,i1)") ifile/100,mod(ifile,100)/10,mod(ifile,10)
         dumpfile = rootname(1:4)//fileno 
-	elseif (len_trim(rootname).eq.5) then
+     elseif (len_trim(rootname).eq.5) then
         write(fileno,"(i1,i1)") ifile/10,mod(ifile,10)
         dumpfile = rootname(1:5)//trim(fileno)     
      endif
@@ -90,7 +96,7 @@ subroutine read_data(rootname,istart,ifinish)
   ndim = 3
   ndimV = 3
   if (magfield) then
-     ncolumns = 19	! number of columns in file
+     ncolumns = 19  ! number of columns in file
   else 
      ncolumns = 11
   endif
@@ -127,48 +133,64 @@ subroutine read_data(rootname,istart,ifinish)
 !
 !--allocate/reallocate memory if j > maxstep
 !
-	if (j.gt.maxstep) then
-	   call alloc(maxpart,j+10,maxcol)
-	endif
+        if (j.gt.maxstep) then
+           call alloc(maxpart,j+10,maxcol)
+        endif
 !
 !--allocate a temporary array for double precision variables
 !
         if (allocated(dattemp)) deallocate(dattemp)
-	allocate(dattemp(ntotin,ncol_max))
+        allocate(dattemp(ntotin,ncol_max))
+!
+!--allocate a dummy arrays for data I want to throw away
+!
+        if (allocated(dummy)) deallocate(dummy)
+        allocate(dummy(ntotin))
+        if (allocated(isteps)) deallocate(isteps)
+        allocate(isteps(ntotin))
+        if (allocated(iphase)) deallocate(iphase)
+        allocate(iphase(ntotin))
 !
 !--now read the timestep data in the dumpfile
 !
         read(15,end=55,err=56) udisti, umassi, utimei, umagfdi,  &
-	     nprint, nghosti, n1, n2, timei, gammai, rhozero, RK2, &
-	     (dattemp(i,7), i=1, nprint), (dattemp(i,8), i=1,nprint), &
-	     escap, tkin, tgrav, tterm, tmag, &
-	     (dattemp(i,1), i=1, nprint), (dattemp(i,2), i=1, nprint), &
-	     (dattemp(i,3), i=1, nprint), (dattemp(i,4), i=1, nprint), &
-	     (dattemp(i,5), i=1, nprint), (dattemp(i,6), i=1, nprint), &
-	     (dattemp(i,9), i=1, nprint), (dattemp(i,10), i=1, nprint), &
-	     (dattemp(i,11), i=1, nprint), (dattemp(i,12), i=1, nprint), &  
-	     (dattemp(i,13), i=1, nprint), (dattemp(i,14), i=1, nprint), &
-	     (dattemp(i,15), i=1, nprint), (dattemp(i,16), i=1, nprint), &
-	     (dattemp(i,17), i=1, nprint), (dattemp(i,18), i=1, nprint), &
-	     (dattemp(i,19), i=1, nprint)
+             nprint, nghosti, n1, n2, timei, gammai, rhozero, RK2, &
+             (dattemp(i,7), i=1, nprint), (dattemp(i,8), i=1,nprint), &
+             escap, tkin, tgrav, tterm, tmag, &
+             (dattemp(i,1), i=1, nprint), (dattemp(i,2), i=1, nprint), &
+             (dattemp(i,3), i=1, nprint), (dattemp(i,4), i=1, nprint), &
+             (dattemp(i,5), i=1, nprint), (dattemp(i,6), i=1, nprint), &
+             (dattemp(i,9), i=1, nprint), (dattemp(i,10), i=1, nprint), &
+             (dattemp(i,11), i=1, nprint), (dattemp(i,12), i=1, nprint), &  
+             (dattemp(i,13), i=1, nprint), (dattemp(i,14), i=1, nprint), &
+             (dattemp(i,15), i=1, nprint), (dattemp(i,16), i=1, nprint), &
+             (dattemp(i,17), i=1, nprint), (dattemp(i,18), i=1, nprint), &
+             (dattemp(i,19), i=1, nprint), (dummy(i),i=1,nprint), &
+             dtmax, (isteps(i), i=1,nprint), (iphase(i),i=1,nprint), &
+             nptmass, (listpm(i), i=1,nptmass)
 !
 !--convert to single precision
-!     
-	print *,'step ',j,': ntotal = ',nprint
-	print "(a)",' converting to single precision... '
-	dat(1:nprint,1:ncol_max,j) = real(dattemp(1:nprint,1:ncol_max))
-	deallocate(dattemp)
+!
+        print *,'step ',j,': ntotal = ',nprint
+        print "(a)",' converting to single precision... '
+        dat(1:nprint,1:ncol_max,j) = real(dattemp(1:nprint,1:ncol_max))
 
-	ntot(j) = nprint
-	nghost(j) = nghosti
-	npart(j) = ntot(j) - nghost(j)
-	npartoftype(1,j) = npart(j)
-	npartoftype(2,j) = nghost(j)
-	  
-	gamma(j) = real(gammai)
-	time(j) = real(timei)
-	if (ntotin.eq.130000) ntotin = nprint
-	j = j + 1
+        iam(1:nprint,j) = iphase(1:nprint)
+        deallocate(dattemp)
+        deallocate(dummy)
+        deallocate(isteps)
+        deallocate(iphase)
+
+        ntot(j) = nprint
+        nghost(j) = nghosti
+        npart(j) = ntot(j) - nghost(j)
+        npartoftype(1,j) = npart(j)
+        npartoftype(2,j) = nghost(j)
+
+        gamma(j) = real(gammai)
+        time(j) = real(timei)
+        if (ntotin.eq.130000) ntotin = nprint
+        j = j + 1
 
      enddo over_steps_in_file
 
@@ -228,10 +250,10 @@ subroutine set_labels
   enddo
   ivx = 4
   ivlast = 6
-  irho = 18		! location of rho in data array
-  iutherm = 16	!  thermal energy
-  ih = 7		!  smoothing length
-  ipmass = 17	!  particle mass      
+  irho = 18     ! location of rho in data array
+  iutherm = 16  !  thermal energy
+  ih = 7        !  smoothing length
+  ipmass = 17   !  particle mass      
   
   label(ix(1:ndim)) = labelcoord(1:ndim,1)
   do i=1,ndimV
@@ -246,17 +268,17 @@ subroutine set_labels
   
   label(ndim + ndimV+5) = '\ga'
   if (ncolumns.gt.11) then
-     iBfirst = 9	! location of Bx
-     iBlast = 11	! location of Bz      
+     iBfirst = 9    ! location of Bx
+     iBlast = 11    ! location of Bz      
      do i=1,ndimV
-	label(iBfirst + i-1) = 'B\d'//labelcoord(i,1) !' (x10\u-3\d)'	!//'/rho'
+        label(iBfirst + i-1) = 'B\d'//labelcoord(i,1) !' (x10\u-3\d)'	!//'/rho'
      enddo
      idivB = 12
      label(idivB) = 'div B'
      do i=1,ndimV
-	label(13 + i-1) = 'J'//labelcoord(i,1)
+        label(13 + i-1) = 'J'//labelcoord(i,1)
      enddo
-  else	 
+  else
      iBfirst = 0
      iBlast = 0
   endif
