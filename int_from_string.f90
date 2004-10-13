5b404f76bafb4b1ccbe0ccad93a91ec66f1bdecc
dprice <dprice>
2004-10-13 14:38:41 +0000

    tabs removed

--- int_from_string.f90
+++ int_from_string.f90
@@ -20,14 +20,14 @@ INTEGER FUNCTION int_from_string(string)
  izero = IACHAR('0')    ! position of zero in ASCII character set
  negative = .false.
 
- DO i=maxdigits,1,-1	! down through the characters
+ DO i=maxdigits,1,-1    ! down through the characters
     IF (string(i:i).NE.' ' .AND. string(i:i).NE.'-') THEN
        ipower = ipower + 1
        idigit = IACHAR(string(i:i)) - izero
        IF ((idigit.LT.0).OR.(idigit.GT.10)) THEN
           !PRINT*, 'int_from_string: non-numeric character in input string'
-	  int_from_string = 0
-	  RETURN
+          int_from_string = 0
+          RETURN
        ELSE
           int_from_string = int_from_string + idigit*10**ipower
        ENDIF
