5c745ab48ce3988d58aa73273be5d7b3eb7c4927
dprice <dprice>
2005-08-11 14:37:28 +0000

    more files allowed

--- globaldata.f90
+++ globaldata.f90
@@ -29,7 +29,7 @@ end module particle_data
 !
 module filenames
  implicit none
- integer, parameter :: maxfile = 501
+ integer, parameter :: maxfile = 1001
  integer :: nfiles,nstepstotal,ifileopen
  character(len=120), dimension(maxfile) :: rootname
  integer, dimension(maxfile) :: nstepsinfile
