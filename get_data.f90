a1102126de681cee3ed45a47a9bdfd7cf2dbb499
dprice <dprice>
2006-01-05 13:40:30 +0000

    bug fix in unit labelling (after coord change)

--- get_data.f90
+++ get_data.f90
@@ -211,7 +211,10 @@ subroutine get_data(ireadfile,gotfilenames,firsttime)
            icoordsnew = 1
         endif
      endif
-     label(1:ndim) = labelcoord(1:ndim,icoordsnew)
+     do i=1,ndim
+        label(i) = labelcoord(i,icoordsnew)
+        if (iRescale .and. icoords.eq.icoordsnew) label(i) = trim(label(i))//unitslabel(i)
+     enddo
      do i=1,ncolumns+ncalc
         if (iamvec(i).ne.0) then
            label(i) = trim(labelvec(iamvec(i)))//'\d'//labelcoord(i-iamvec(i)+1,icoordsnew)
