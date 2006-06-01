0e24312a7f843998259dabf4447d3a4337da88ae
dprice <dprice>
2006-06-01 13:10:09 +0000

    script for getting average of L2 errors

---
+++ scripts/getav.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+# adds up the L2 errors from supersphplot output; calculates average
+use strict;
+use warnings;
+
+use List::Util qw(sum);
+
+my $file = 'plummerMC.output';
+
+open my $fh, '<', $file or die "Can't open $file: $!";
+my @errors;
+
+while ( <$fh> ) {
+    my ($error) = m/L2 error\s+=\s+(\S*)\s+/;
+    push @errors, $error;
+}
+
+my $avg = sum(@errors) / scalar(@errors);
+
+print "Average error: $avg\n";
+
+#print "$_\n" for @errors;
