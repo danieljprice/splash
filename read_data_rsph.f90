ce11f4bac74da9a8a039b06c17cc58d3d3fd2056
dprice <dprice>
2006-07-20 10:16:19 +0000

    updated rsph read

--- read_data_rsph.f90
+++ read_data_rsph.f90
@@ -44,9 +44,9 @@ subroutine read_data(rootname,indexstart,nstepsread)
   integer :: ntoti
   logical :: singleprecision
 
-  integer(kind=4), dimension(10) :: sheader
+  integer*2, dimension(10) :: sheader
   integer, dimension(10) :: iheader
-  integer(kind=8), dimension(10):: lheader
+  integer*8, dimension(10):: lheader
   real, dimension(10) :: rheader
   real(doub_prec), dimension(10) :: dheader
   character(len=100) :: headerstring
@@ -78,15 +78,16 @@ subroutine read_data(rootname,indexstart,nstepsread)
 !--read first header line
 !
   read(iunit,iostat=ierr,end=80) headerstring
-  print "(a)",trim(headerstring)
+  print "(1x,a)",trim(headerstring)
 !
 !--read other header lines (short/normal/long ints, reals, doubles)
 !
-  read(iunit,iostat=ierr,end=80) sheader(1),sheader(1:max(size(sheader),sheader(1)))
+  read(iunit,iostat=ierr,end=80) sheader(1:10)
   if (ierr /= 0) then
      print*,'WARNING: errors during sheader read'
   endif
-  print*,'sheader = ',sheader
+  print "(1x,a,10(1x,i2))",'sheader = ',sheader(1:10)
+
   isizeheader = sheader(1)
   ndim = sheader(2)
   ndimV = sheader(3)
@@ -105,14 +106,16 @@ subroutine read_data(rootname,indexstart,nstepsread)
   if (ierr /= 0) then
      print*,'WARNING: errors during iheader read'
   endif
-  print*,'iheader = ',iheader
+  print "(1x,a,20(1x,i6))",'iheader = ',iheader(1:isizeheader)
+
   ntoti = sum(iheader(1:ntypes))
   
   read(iunit,iostat=ierr,end=80) lheader(1:isizeheader)  
   if (ierr /= 0) then
      print*,'WARNING: errors during lheader read'
   endif
-  print*,'lheader = ',lheader
+  print "(1x,a,20(1x,i6))",'lheader = ',lheader(1:isizeheader)
+
   read(iunit,iostat=ierr,end=80) rheader(1:isizeheader)
   if (ierr /= 0) then
      print*,'WARNING: errors during rheader read'
@@ -154,13 +157,13 @@ subroutine read_data(rootname,indexstart,nstepsread)
   singleprecision = .true.
   if (all(abs(rheader(1:isizeheader)).lt.tiny(rheader))) then
      singleprecision = .false.
-     print "(a)",'double precision dump'
+     print "(1x,a)",'double precision dump'
      time(i) = real(dheader(1))
      print*,'particles per smoothing length = ',dheader(2)
      print*,'kernel range = ',dheader(3)
      gamma(i) = real(dheader(4))
   else
-     print "(a)",'single precision dump'
+     print "(1x,a)",'single precision dump'
      time(i) = rheader(1)
      print*,'particles per smoothing length = ',rheader(2)
      print*,'kernel range = ',rheader(3)
@@ -204,7 +207,7 @@ subroutine read_data(rootname,indexstart,nstepsread)
      if (ierr /=0 ) then
         print "(a)",'error reading particle positions'
      else
-        do icol=1,3
+        do icol=1,ndim
            dat(1:ntoti,icol,i) = real(dattempd(icol,1:ntoti))
         enddo
      endif
@@ -247,6 +250,7 @@ subroutine set_labels
  use params
  use settings_data, only:ndim,ndimV,ncolumns,iformat,ntypes, &
                     UseTypeInRenderings
+ use particle_data, only:gamma
  use geometry, only:labelcoord
  implicit none
  integer :: i,icol
@@ -263,11 +267,32 @@ subroutine set_labels
  do i=1,ndim
     ix(i) = i
  enddo
- ivx = ndim + 1
- ih = ndim + ndimV + 1        !  smoothing length
- irho = ndim + ndimV + 2      ! location of rho in data array
- iutherm = ndim + ndimV + 3   !  thermal energy
- ipmass = ndim + ndimV + 4    !  particle mass     
+ ipmass = ndim + 1
+ irho = ndim + 2
+ icol = ndim+3
+ label(icol) = 'P'
+ !if ((gamma(1)-1.0).gt.tiny(gamma(1))) then
+ !   iutherm = ndim+4
+ !   icol = icol + 1
+ !else
+    iutherm = 0
+ !endif
+ icol = icol + 1
+
+ label(icol) = 's'
+ icol = icol + 1
+
+ ih = icol
+ label(ih) = 'h'
+ icol = icol + 1
+
+ label(icol) = 'time bin #'
+ icol = icol + 1
+
+ label(icol) = 'type'
+ icol = icol + 1
+
+ ivx = icol
 
  label(ix(1:ndim)) = labelcoord(1:ndim,1)
  !
@@ -278,95 +303,21 @@ subroutine set_labels
  do i=1,ndimV
     label(ivx+i-1) = trim(labelvec(ivx+i-1))//'\d'//labelcoord(i,1)
  enddo
+
  
  label(irho) = '\gr'
- label(iutherm) = 'u'
+ if (iutherm.gt.0) label(iutherm) = 'u'
  label(ih) = 'h       '
  label(ipmass) = 'particle mass'
- label(ndim + ndimV+5) = '\ga'
- label(ndim + ndimV+6) = '\ga\du'
- icol = ndim+ndimV + 7
- if (iformat.eq.2 .or. iformat.eq.4) then
-    !
-    !--mag field (vector)
-    !
-    label(icol) = '\ga\dB'
-    iBfirst = icol+1        ! location of Bx
-    iamvec(iBfirst:iBfirst+ndimV-1) = iBfirst
-    labelvec(iBfirst:iBfirst+ndimV-1) = 'B'
-    do i=1,ndimV
-       label(iBfirst+i-1) = trim(labelvec(iBfirst))//'\d'//labelcoord(i,1) !' (x10\u-3\d)' !//'/rho'
-    enddo
-    icol = icol + ndimV
-    !
-    !--more scalars
-    !
-    icol = icol + 1
-    label(icol) = 'psi'
-    
-    icol = icol + 1
-    ipr = icol !  pressure
-    label(ipr) = 'P'
-    
-    icol = icol + 1
-    label(icol) = 'div v'
-
-    icol = icol + 1
-    idivB = icol
-    label(idivB) = 'div B'
-    !
-    !--current density (vector)
-    !
-    iJfirst = icol + 1
-    iamvec(icol+1:icol+ndimV) = icol + 1
-    labelvec(icol+1:icol+ndimV) = 'J'
-    icol = icol + ndimV
-    
-    icol = icol + 1
-    label(icol) = 'grad h'
-    
-    iamvec(icol+1:icol+ndimV) = icol + 1
-    labelvec(icol+1:icol+ndimV) = 'force'
-    icol = icol + ndimV
-
- else
-    ipr = icol !  pressure
-    label(ipr) = 'P'
-    icol = icol + 1
-    label(icol) = 'div v'
-    icol = icol + 1    
-    label(icol) = 'grad h'
-
-    iamvec(icol+1:icol+ndimV) = icol + 1
-    labelvec(icol+1:icol+ndimV) = 'force'
-    icol = icol + ndimV
-!    do i=1,ndimV
-!       label(ndim+ndimV+9+i) = labelvec(ndim+2*ndimV+ndimV)//labelcoord(i,1)
-!    enddo
-
-    iBfirst = 0
- endif
- if (iformat.gt.2) then
-    !!!irho = ndim+ndimV+9
-    icol = icol + 1
-    label(icol) = 'rho*'
-    icol = icol + 1
-    label(icol) = 'sqrt g'
-    iamvec(icol+1:icol+ndimV) = icol + 1
-    labelvec(icol+1:icol+ndimV) = 'pmom'
-    do i=1,ndimV
-       label(icol+i) = labelvec(icol+i)//labelcoord(i,1)
-    enddo
-    icol = icol + ndimV
- endif
 !
 !--set labels for each type of particles
 !
- ntypes = 2
  labeltype(1) = 'gas'
- labeltype(2) = 'ghost'
+ labeltype(2) = 'auxiliary'
+ labeltype(3) = 'mirror'
  UseTypeInRenderings(1) = .true.
  UseTypeInRenderings(2) = .true.
+ UseTypeInRenderings(3) = .true.
  
 !-----------------------------------------------------------
 
