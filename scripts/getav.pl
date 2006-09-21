90d9ec02245c54d9a5136c332280074b1b4e8666
dprice <dprice>
2006-09-21 12:49:00 +0000

    bug fix with error parsing script

--- scripts/getav.pl
+++ scripts/getav.pl
@@ -6,14 +6,19 @@ use warnings;
 use List::Util qw(sum);
 my $file = ' ';
 my $avg = 0;
+if ($#ARGV < 0) {
+   print "script which parses supersphplot output for L2 errors \n";
+   print "and calculates the average. Written by D. Price. \n\n";
+   die "Usage: $0 filename(s) \n";
+}
 
 foreach $file (@ARGV) {
    open my $fh, '<', $file or die "Can't open $file: $!";
    my @errors;
 
    while ( <$fh> ) {
-      if (-m 'L2 error') {
-         my ($error) = m/L2 error\s+=\s+(\S*)\s+/;
+      if ( my ($error) = m/L2 error\s+=\s+(\S*)\s+/ ) {
+         #print "error = $error \n";
          push @errors, $error;
       }
    }
