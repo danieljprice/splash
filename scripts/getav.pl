9b27f2c377dd417a6d7c9208bb4967709b57cad5
dprice <dprice>
2006-09-21 12:16:38 +0000

    minor improvements

--- scripts/getav.pl
+++ scripts/getav.pl
@@ -5,19 +5,28 @@ use warnings;
 
 use List::Util qw(sum);
 my $file = ' ';
+my $avg = 0;
 
 foreach $file (@ARGV) {
    open my $fh, '<', $file or die "Can't open $file: $!";
    my @errors;
 
    while ( <$fh> ) {
-       my ($error) = m/L2 error\s+=\s+(\S*)\s+/;
-       push @errors, $error;
+      if (-m 'L2 error') {
+         my ($error) = m/L2 error\s+=\s+(\S*)\s+/;
+         push @errors, $error;
+      }
    }
 
-   my $avg = sum(@errors) / scalar(@errors);
+   my $nerrors = scalar(@errors);
+   if ($nerrors > 0) {
+      $avg = sum(@errors) / $nerrors;
+   }
+   else {
+      $avg = 0;   
+   }
 
-   print "$file: Average error: $avg\n";
+   print "$file: Average of $nerrors errors: $avg\n";
 }
 
 #print "$_\n" for @errors;
