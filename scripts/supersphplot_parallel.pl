24ac6a21d830f9346d115431d434d243690ab77b
dprice <dprice>
2006-10-16 14:53:10 +0000

    working ssh version

--- scripts/supersphplot_parallel.pl
+++ scripts/supersphplot_parallel.pl
@@ -5,32 +5,48 @@
 use strict;
 use warnings;
 use IO::Handle;
-
-my $run;
-my $home=`cd; pwd -P`;
-chomp ($home);
-my $exe="$home/ndspmhd/plot/ssupersphplot";
-my $inputfile='input';
-my $pgplotdev='none';
-my $pgplotfile='pgplot';
-my $pgplotdir= $ENV{'PGPLOT_DIR'}; # get this from the environment
-
+#---------------------------------------------------------
+#  check args
+#---------------------------------------------------------
 if ($#ARGV < 1 ) {
    die "Usage: $0 nfilesperplot file1 [file2 file3 ... filen] \n also with a file called input \n";
 }
-
-my ($nfilesperplot,@files) = @ARGV;
+#---------------------------------------------------------
+#  set farming method options are ssh, xgrid
+#---------------------------------------------------------
+my $farmusing='ssh';
+#my $farmusing='xgrid';
+#---------------------------------------------------------
+#  set name and location of the supersphplot executable
+#---------------------------------------------------------
+my $home=`cd; pwd -P`;
+chomp ($home);
+my $exe="$home/ndspmhd/plot/ssupersphplot";
 my $pwd = `pwd`;
 chomp($pwd);
-
+#---------------------------------------------------------
+#  set other default options
+#---------------------------------------------------------
+my $inputfile='input';
+my $pgplotfile='pgplot';
+my $pgplotdev='none'; # doesn't matter
+my $pgplotdir= $ENV{'PGPLOT_DIR'}; # get this from the environment
+my $ldpath= $ENV{'LD_LIBRARY_PATH'}; # get this from the environment
+my $run;
+#---------------------------------------------------------
+#  get filenames and nfilesperplot from command line
+#---------------------------------------------------------
+my ($nfilesperplot,@files) = @ARGV;
+#---------------------------------------------------------
+#  work out how many files should be sent as command line arguments to each run
+#---------------------------------------------------------
 if ($nfilesperplot < 1) { die "error: nfilesperplot must be > 0\n" };
-
 my $nfiles=$#files + 1;
 my $nruns=$nfiles/$nfilesperplot;
 print "nfiles = $nfiles; nruns = $nruns\n";
-#
-#  get input from input file
-#
+#---------------------------------------------------------
+#  get generic input from input file
+#---------------------------------------------------------
 my @inputs = `cat $inputfile` or die "error: input file does not exist\n";
 #open my $fh,'<',$inputfile or die "error: input file does not exist\n";
 my $line;
@@ -45,26 +61,32 @@ foreach $line (@inputs) {
 }
 #close $fh;
 
-#
-#--work out what device we are writing to and therefore what the filenames
-#  should be
-#
+#---------------------------------------------------------
+#  work out what device we are writing to and therefore 
+#  what the filenames should be
+#---------------------------------------------------------
+
 print "PGPLOT device = $pgplotdev $inputs[$devline]\n";
 if ( $pgplotdev eq '/xw' or $pgplotdev eq '/aqt' ) { 
    die "must choose a non-interactive PGPLOT device\n";
 }
+#---------------------------------------------------------
+#  work out the filename extension from the device name
+#---------------------------------------------------------
 my ($ext) = $pgplotdev =~ m|/(.+)|;
 #print "extension = $ext \n";
 
-#
-#--farm out the jobs
-#
+#---------------------------------------------------------
+#  farm out the jobs
+#---------------------------------------------------------
 my $filestart = 0;
 my $fileend = $filestart + $nfilesperplot;
+my $n = 0;
 for ($run=1;$run<=$nruns;$run++) {
     my @inputstemp = @inputs;
     my $num = sprintf("%05d",$run);
-    my $pgplotfile = "pgplot_$num.$ext";
+    #my $pgplotfile = "pgplot_$num.$ext";
+    my $pgplotfile = "pgplot.$ext\_$run";
     $inputstemp[$devline] = "$pgplotfile$pgplotdev\n";
     print "------------ run $run : $pgplotfile ------------\n";
     my @argsn=@files[$filestart..$fileend-1];
@@ -79,17 +101,32 @@ for ($run=1;$run<=$nruns;$run++) {
     open(RUNSCR,"> run$run.csh") || die("can't write run script");
     print RUNSCR "#!/bin/tcsh \n";
     print RUNSCR "setenv PGPLOT_DIR $pgplotdir \n";
+    print RUNSCR "setenv LD_LIBRARY_PATH $ldpath \n";
     print RUNSCR "cd $pwd \n";
     print RUNSCR "$exe @argsn < input$run >& run$run.output \n";
     close(RUNSCR);
     system "chmod a+x run$run.csh";
     my $commandline = "./run$run.csh";
-    farmjob_xgrid( $commandline );
+    if ( -s $pgplotfile ) {
+       print "skipping $pgplotfile which already exists \n";
+    } else {
+      if ($farmusing eq 'ssh') {
+         farmjob_ssh( $commandline, $n );    
+      } elsif ($farmusing eq 'xgrid') {
+         farmjob_xgrid( $commandline );
+      } else {
+         die "unknown farming method \n";
+      }
+    }
 #---------------------------------
     $filestart = $fileend;
     $fileend = $filestart + $nfilesperplot;
 }
 exit;
+
+#---------------------------------------------------------
+#   subroutine to farm job via Apple's Xgrid
+#---------------------------------------------------------
 
 sub farmjob_xgrid {
     my $commandline=shift;
@@ -102,43 +139,59 @@ sub farmjob_xgrid {
     sleep 1; # avoid multiple rapid-fire requests to xgrid server
 }
 
+#---------------------------------------------------------
+#   subroutine to farm jobs via simple ssh commands
+#   to a list of machines
+#---------------------------------------------------------
+
 sub farmjob_ssh {
-    my ($commandline,$runs)=@_;
-    my (@machines) = `cat machinelist` or die "ERROR: for ssh version must list machines in file machinelist \n";
+    my ($commandline,$n)=@_;
+#    my (@machines) = ('perky','tintin','homer','donald','butch','goofy','fred');
+    my (@machines) = ('perky','tintin','obelix','homer','daphne','velma','taz','wallace','popeye','donald',
+       'goofy','zippy','fred','butch');
+
+    ##my (@machines) = `cat machinelist` or die "ERROR: for ssh version must list machines in file machinelist \n";
     my $nmachines = $#machines;
     if ( $nmachines < 1 ) {
        die "ERROR: no machines specified in file machinelist \n";
     }
+    my $pwd = `pwd`;
+    chomp($pwd);
 
-    my $machine;
     my $njobsrun = 0;
     my $loadavmax = 0.5;
-    my $n;
+    my $bs = '\\';
 
     # loop through all available machines looking for spare CPU
-    while ($njobsrun < $runs) {
-        
+    while ($njobsrun < 1) {
+        #--set which machine to try
+        if ($n > $nmachines) { 
+           print "waiting for machines to become free \n";
+           sleep 30;
+           $n = 0;
+        };
+        my $machine = $machines[$n];
         chomp($machine);
-        my $loadav = 0.;
-        print "----------------- \n trying $machine \n";
-    #    get load average for this machine using uptime command
+        
+        my $loadav = 0.0;
+        print "trying $machine: ";
+        #--get load average for this machine using uptime command
         if ($loadavmax > 0.0) {
            my $uptime=`ssh $machine uptime`;
-           ($loadav) = $uptime =~ m/load averages:(\d+\.\d+)\s/;
-           print " load average last 1 minute = $loadav";
+           ($loadav) = $uptime =~ m/load ave.+:\s(\d+\.\d+)./;
+           print " load average last 1 minute = $loadav ";
         }
+        #--set job running if not busy
         if ( $loadav < $loadavmax ) {
            print " ...OK \n";
            $njobsrun = $njobsrun + 1;
            # run the job
-    #           print "running $rootname$njobsrun on machine $machine at nice +19\n";
-    #           system "ssh $machine 'cd $pwd/$rootname; nice +19 ./$ndim$SPMHD $rootname$njobsrun > $rootname$njobsrun.output &' ";
+           print "running $commandline on machine $machine at nice +19\n";
+           system "ssh -f $machine 'cd $pwd; nice +19 $commandline < /dev/null >& /dev/null &'";
         } else {
            print " ...too busy \n";
         }
-
+        $n = $n + 1;
     }
-    print "===========================================\n";
-    print "\n Hurrah! all jobs successfully submitted \n";
-    print " $njobsrun jobs run \n";
+    $_[1] = $n;
 }
