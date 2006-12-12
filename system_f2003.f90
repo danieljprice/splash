f230a62ab8a206c76efb8e2f4f0ac4547fd3646b
dprice <dprice>
2006-12-12 02:38:24 +0000

    environment variable reading added to system commands

--- system_f2003.f90
+++ system_f2003.f90
@@ -26,5 +26,33 @@ contains
     call GET_COMMAND_ARGUMENT(iarg,argstring)
         
  end subroutine get_argument
+
+ subroutine get_environment(variable,value)
+    character(len=*), intent(in) :: variable
+    character(len=*), intent(out) :: value
+ 
+    call GET_ENVIRONMENT_VARIABLE(variable,value)
+ 
+ end subroutine get_environment
+
+ !--this routine returns a logical variable
+ !  from an environment variable setting
+ !  and should not require modification
+ ! (just acts as an interface to get_environment)
+ !
+ logical function lenvironment(variable)
+    character(len=*), intent(in) :: variable
+    character(len=10) :: string
+    
+    call get_environment(variable,string)
+    if (trim(string).eq.'yes'.or.trim(string).eq.'YES' &
+    .or.trim(string).eq.'true'.or.trim(string).eq.'TRUE' &
+    .or.trim(string).eq.'on'.or.trim(string).eq.'ON') then
+       lenvironment = .true.
+    else
+       lenvironment = .false.
+    endif
+ 
+ end function lenvironment
  
 end module system_commands
