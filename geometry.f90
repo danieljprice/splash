8dab96d841d8d799295471faa8a1cc2e1d125143
dprice <dprice>
2007-02-06 09:08:35 +0000

    BUG FIX with phi component of vectors in cylindrical geometry

--- geometry.f90
+++ geometry.f90
@@ -366,7 +366,7 @@ subroutine vector_transform(xin,vecin,ndimin,itypein,vecout,ndimout,itypeout)
         !
         !--output is cylindrical
         !
-        rr = sqrt(dot_product(xin(1:max(ndimin,2)),xin(1:max(ndimin,2))))
+        rr = sqrt(dot_product(xin(1:min(ndimin,2)),xin(1:min(ndimin,2))))
         if (rr.ne.0.) then
            rr1 = 1./rr
         else
