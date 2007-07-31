847a27be858fc359d8b81d66eba87bd0ed2ca099
dprice <dprice>
2007-07-31 10:00:39 +0000

    ienvironment function added

--- system_utils.f90
+++ system_utils.f90
@@ -5,9 +5,34 @@
 module system_utils
  use system_commands, only:get_environment
  implicit none
- public :: lenvironment,renvironment
+ public :: ienvironment,lenvironment,renvironment
 
 contains
+ !
+ !--this routine returns an integer variable
+ !  from an environment variable setting
+ !
+ !  if the errval argument is present then this is the
+ !  value assigned when an error has occurred
+ !  (default is zero)
+ !
+ integer function ienvironment(variable,errval)
+    character(len=*), intent(in) :: variable
+    character(len=30) :: string
+    integer, intent(in), optional :: errval
+    integer :: ierr
+    
+    call get_environment(variable,string)
+    read(string,*,iostat=ierr) ienvironment
+    if (ierr /= 0) then
+       if (present(errval)) then
+          ienvironment = errval 
+       else
+          ienvironment = 0
+       endif
+    endif
+ 
+ end function ienvironment
 
  !
  !--this routine returns a floating point (real) variable
@@ -44,8 +69,8 @@ contains
     character(len=30) :: string
     
     call get_environment(variable,string)
-    if (trim(string).eq.'yes'.or.trim(string).eq.'YES' &
-    .or.trim(string).eq.'true'.or.trim(string).eq.'TRUE' &
+    if (string(1:1).eq.'y'.or.string(1:1).eq.'Y' &
+    .or.string(1:1).eq.'t'.or.string(1:1).eq.'T' &
     .or.trim(string).eq.'on'.or.trim(string).eq.'ON') then
        lenvironment = .true.
     else
