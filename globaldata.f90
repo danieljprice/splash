bc299e9b48bddfc2298d8fe930ab27f8c88b3d23
dprice <dprice>
2009-07-15 05:15:37 +0000

    single precision defined; bug fix with int8 definition

--- globaldata.f90
+++ globaldata.f90
@@ -9,8 +9,9 @@
 module params
  implicit none
  integer, parameter :: doub_prec = selected_real_kind(P=10,R=30)
+ integer, parameter :: sing_prec = selected_real_kind(P=5,R=15)
  integer, parameter :: int1 = selected_int_kind(1)
- integer, parameter :: int8 = selected_int_kind(9)
+ integer, parameter :: int8 = selected_int_kind(12)
  integer, parameter :: maxplot=64   ! maximum number of plots (for multiplot arrays)
  integer, parameter :: maxparttypes = 6  ! max # of different particle types
 
