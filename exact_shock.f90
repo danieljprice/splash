6edd60919f0e7d7ac026c7781402f386be3ef142
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2009-08-07 00:08:13 +0000

    header notice added to all .f90 files
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/splash@12 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- exact_shock.f90
+++ exact_shock.f90
@@ -1,3 +1,25 @@
+!-----------------------------------------------------------------
+!
+!  This file is (or was) part of SPLASH, a visualisation tool 
+!  for Smoothed Particle Hydrodynamics written by Daniel Price:
+!
+!  http://users.monash.edu.au/~dprice/splash
+!
+!  SPLASH comes with ABSOLUTELY NO WARRANTY.
+!  This is free software; and you are welcome to redistribute
+!  it under the terms of the GNU General Public License
+!  (see LICENSE file for details) and the provision that
+!  this notice remains intact. If you modify this file, please
+!  note section 2a) of the GPLv2 states that:
+!
+!  a) You must cause the modified files to carry prominent notices
+!     stating that you changed the files and the date of any change.
+!
+!  Copyright (C) 2005-2009 Daniel Price. All rights reserved.
+!  Contact: daniel.price@sci.monash.edu.au
+!
+!-----------------------------------------------------------------
+
 ! ----------------------------------------------------------------------
 ! compute exact solution for the one dimensional Riemann problem
 ! (hydrodynamic shock)
