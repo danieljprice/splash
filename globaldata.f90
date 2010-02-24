1d0157a7d1d1d36202e94defcf05087241833c26
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2010-02-24 17:03:58 +0000

    lenunitslabel added
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/splash@862 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- globaldata.f90
+++ globaldata.f90
@@ -105,6 +105,7 @@ module labels
  use params
  implicit none
  integer, parameter :: lenlabel = 60
+ integer, parameter :: lenunitslabel = 20  ! length of units label
  character(len=lenlabel), dimension(maxplot+2) :: label,labelvec
  character(len=20), dimension(maxparttypes) :: labeltype
  character(len=6), parameter :: labeldefault = 'column'
