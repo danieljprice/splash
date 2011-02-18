163fdc87ba3734724c429316b34db6dcef91c978
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-02-18 04:38:53 +0000

    updated grid2pdf utility
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/splash@1395 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- utils/grid2pdf.f90
+++ utils/grid2pdf.f90
@@ -1,15 +1,15 @@
 program grid2pdf
  use system_commands,    only:get_number_arguments,get_argument
  use readwrite_griddata, only:open_gridfile_r,read_gridcolumn
- use pdfs,               only:pdf_calc,pdf_write
+ use pdfs,               only:pdf_calc,pdf_write,mean_variance
  implicit none
  logical, parameter     :: usefixedbins = .true.
  integer, parameter     :: nbins = 270
  integer, parameter     :: iunit = 13
  integer, dimension(3)  :: nxgrid
  integer                :: ierr,ncolumns,itransx,ifile,nargs
- integer                :: ntot
- real                   :: time,rhologmin,rhologmax,pdfmin,pdfmax
+ integer                :: ntot,i
+ real                   :: time,rhologmin,rhologmax,pdfmin,pdfmax,smean,svar,rhomean,rhovar
  real, dimension(nbins) :: xbin,pdf
  character(len=120)     :: filename,tagline
  character(len=20)      :: informat
@@ -52,6 +52,11 @@ program grid2pdf
     !
     close(iunit)
     !
+    !--calculate the mean and variance in rho
+    !
+    call mean_variance(rhogrid,ntot,rhomean,rhovar)
+
+    !
     !--set the parameters for calculating the PDF
     !  [in this case, we want the PDF of ln (rho)]
     !
@@ -60,17 +65,35 @@ program grid2pdf
     where (rhogrid.gt.0.)
        rhogrid = log(rhogrid)
     end where
-    itransx = 6
+    !itransx = 6
     !
     !--calculate the PDF
     !
     call pdf_calc(ntot,rhogrid,rhologmin,rhologmax,nbins,xbin,&
-                  pdf,pdfmin,pdfmax,itransx,usefixedbins,volweighted,ierr)
+                  pdf,pdfmin,pdfmax,usefixedbins,volweighted,ierr)
+    !
+    !--calculate the mean and variance in ln(rho)
+    !
+    call mean_variance(rhogrid,ntot,smean,svar)
+    
+    print*,'    rho mean,var = ',rhomean,rhovar
+    print*,'ln(rho) mean,var = ',smean,svar
+    print*,'    svar,rhovar = ',-2.*smean,exp(svar) - 1.
+    !
+    !--check the routine
+    !
+    svar = 0.
+    do i=1,ntot
+       svar = svar + (rhogrid(i) - smean)**2
+    enddo
+    svar = svar/real(ntot-1)
+    print*,'svar = ',svar
+    
     !
     !--write the PDF to file
     !
     if (ierr.eq.0) then
-       call pdf_write(nbins,xbin,pdf,'rhogrid',itransx,volweighted,trim(filename),trim(tagline))
+       call pdf_write(nbins,xbin,pdf,'lnrhogrid',volweighted,trim(filename),trim(tagline))
     endif
  enddo
  !
