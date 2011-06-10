b48df89ef8878d7882a57eda95726711058b52fb
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-06-10 01:32:07 +0000

    header updated
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/splash@1477 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/asciiutils.f90
+++ src/asciiutils.f90
@@ -15,8 +15,8 @@
 !  a) You must cause the modified files to carry prominent notices
 !     stating that you changed the files and the date of any change.
 !
-!  Copyright (C) 2005-2009 Daniel Price. All rights reserved.
-!  Contact: daniel.price@sci.monash.edu.au
+!  Copyright (C) 2005-2011 Daniel Price. All rights reserved.
+!  Contact: daniel.price@monash.edu
 !
 !-----------------------------------------------------------------
 
@@ -26,7 +26,7 @@
 !
 ! written by Daniel Price, University of Exeter 2007 24th April '07
 ! revised at Monash University, Nov '08.
-! daniel.price@sci.monash.edu.au
+! daniel.price@monash.edu
 !
 ! this is a standalone module with no dependencies
 !---------------------------------------------------------------------------
