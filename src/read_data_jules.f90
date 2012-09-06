7b1642b34f2e64b843fc6145c192f4526f7d696e
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2012-09-06 22:47:43 +0000

    updated jules data read to handle his latest format
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/splash@4758 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/read_data_jules.f90
+++ src/read_data_jules.f90
@@ -15,8 +15,8 @@
 !  a) You must cause the modified files to carry prominent notices
 !     stating that you changed the files and the date of any change.
 !
-!  Copyright (C) 2005-2009 Daniel Price. All rights reserved.
-!  Contact: daniel.price@sci.monash.edu.au
+!  Copyright (C) 2005-2012 Daniel Price. All rights reserved.
+!  Contact: daniel.price@monash.edu
 !
 !-----------------------------------------------------------------
 
@@ -57,8 +57,9 @@ subroutine read_data(rootname,indexstart,nstepsread)
   integer, intent(IN) :: indexstart
   integer, intent(OUT) :: nstepsread
   character(LEN=*), intent(IN) :: rootname
-  integer :: i,j,ifile,ierr,npart,nweird,nbnd
+  integer :: i,j,ifile,ierr,npart,nweird,nbnd,nother,n
   integer :: istep,nprint,npart_max,nstep_max,icol,ncolstep
+  integer :: iambodi,iamskini,imovei
   logical :: iexist
   character(LEN=LEN(rootname)+4) :: dumpfile
   real :: timei,dti,hi,pmass,totmass,rhozero
@@ -69,7 +70,6 @@ subroutine read_data(rootname,indexstart,nstepsread)
   ifile = 1
 
   dumpfile = trim(rootname)
-  if (index(dumpfile,'.plt').eq.0) dumpfile = trim(rootname)//'.plt'
   !
   !--check if first data file exists
   !
@@ -83,8 +83,8 @@ subroutine read_data(rootname,indexstart,nstepsread)
   !
   ndim = 2
   ndimV = 2
-  ncolstep = 7
-  ncolumns = 8  ! number of columns in file
+  ncolstep = 17
+  ncolumns = 18  ! number of columns in file
   !
   !--allocate memory initially
   !
@@ -105,7 +105,8 @@ subroutine read_data(rootname,indexstart,nstepsread)
         !--read the number of particles in the first step,
         !  allocate memory and rewind
         !
-        read(15,*,end=55,iostat=ierr) istep,nprint,hi
+        read(15,*,end=55,iostat=ierr) timei,istep,n,nprint,hi,dti
+        print*,'time = ',timei,' step = ',istep,' n = ',n,' ng = ',nprint
         print*,'first time = ',hi,' npart = ',nprint
         if (.not.allocated(dat) .or. (nprint.gt.npart_max)) then
            npart_max = max(npart_max,INT(1.1*(nprint)))
@@ -117,68 +118,75 @@ subroutine read_data(rootname,indexstart,nstepsread)
         print*,'*** ERROR READING TIMESTEP HEADER ***'
      else
 
-        oversteps: do
+        !oversteps: do
     !
     !--loop over the timesteps in this file
     !
-           npart_max = max(npart_max,nprint)
-    !
-    !--allocate/reallocate memory if j > maxstep
-    !
-           if (j.gt.maxstep) then
-              call alloc(maxpart,j+1,maxcol,mixedtypes=.true.)
-           endif
-    !
-    !--now read the timestep data in the dumpfile
-    !
-           read(15,*,end=55,iostat=ierr) istep,nprint,hi
-			  read(15,*)
-			  rhozero = 1000.
-			  totmass = 3.*4.8*rhozero
-			  pmass = totmass/real(nprint)
-			  print*,' assuming total mass = ',totmass,' (rhozero = ',rhozero,')'
-			  print*,' gives particle mass = ',pmass
+        npart_max = max(npart_max,nprint)
+ !
+ !--allocate/reallocate memory if j > maxstep
+ !
+        if (j.gt.maxstep) then
+           call alloc(maxpart,j+1,maxcol,mixedtypes=.true.)
+        endif
+ !
+ !--now read the timestep data in the dumpfile
+ !
+        read(15,*,end=55,iostat=ierr) timei,istep,n,nprint,hi,dti
+           rhozero = 1000.
+           totmass = 3.*4.8*rhozero
+           pmass = totmass/real(nprint)
+           print*,' assuming total mass = ',totmass,' (rhozero = ',rhozero,')'
+           print*,' gives particle mass = ',pmass
            nbnd = 0
-			  npart = 0
-			  nweird = 0
-			  do i=1,nprint
-              read(15,600,end=55,iostat=ierr) (dat(i,icol,j),icol = 1,ncolstep),iamtype(i,j)
-				  select case(iamtype(i,j))
-				  case(0)
-				     nbnd = nbnd + 1
-					  iamtype(i,j) = 2
-			     case(1)
-				     npart = npart + 1
-					  iamtype(i,j) = 1
-				  case default
-				     nweird = nweird + 1
-					  iamtype(i,j) = 3
-				  end select
-              !print*,i,(dat(i,icol,j),icol = 1,ncolstep),iamtype(i,j)
-				  !--make a fake column for mass
-				  dat(i,8,j) = pmass
+           npart = 0
+           nother = 0
+           nweird = 0
+           do i=1,nprint
+              read(15,*,end=55,iostat=ierr) (dat(i,icol,j),icol = 1,7)
+              read(15,*,end=55,iostat=ierr) (dat(i,icol,j),icol = 8,13)
+              read(15,*,end=55,iostat=ierr) iamtype(i,j),iambodi,iamskini,imovei
+              read(15,*,end=55,iostat=ierr) (dat(i,icol,j),icol = 14,17)
+               select case(iamtype(i,j))
+               case(0)
+                  nbnd = nbnd + 1
+                  iamtype(i,j) = 2
+               case(1)
+                  npart = npart + 1
+                  iamtype(i,j) = 1
+               case(2)
+                  nother = nother + 1
+                  iamtype(i,j) = 3
+               case default
+                  print*,'iamtype = ',iamtype(i,j)
+                  nweird = nweird + 1
+                  iamtype(i,j) = 4
+               end select
+               !print*,i,(dat(i,icol,j),icol = 1,ncolstep),iamtype(i,j)
+               !--make a fake column for mass
+               dat(i,18,j) = pmass
            enddo
-600  	     format(2x,7(e12.5),1(i5))
-			  time(j) = real(istep)
+        time(j) = timei
 
-           if (ierr /= 0) then
-			     print*,'got to ',i,' step ',j
-              print "(a)",'|*** ERROR READING TIMESTEP ***'
-              return
-           else
-              nstepsread = nstepsread + 1
-           endif
+        if (ierr /= 0) then
+           print*,'got to ',i,' step ',j
+           print "(a)",'|*** ERROR READING TIMESTEP ***'
+           return
+        else
+           nstepsread = nstepsread + 1
+        endif
 
-           npartoftype(:,j) = 0
-           npartoftype(1,j) = npart
-           npartoftype(2,j) = nbnd
-           npartoftype(3,j) = nweird
-           if (nweird.gt.0) print*,' WARNING: ',nweird,' particles of unknown type'
-           print*,j,' time = ',time(j)
-           gamma(j) = 1.666666666667
-           j = j + 1
+        npartoftype(:,j) = 0
+        npartoftype(1,j) = npart
+        npartoftype(2,j) = nbnd
+        npartoftype(3,j) = nother
+        npartoftype(4,j) = nweird
+        if (nweird.gt.0) print*,' WARNING: ',nweird,' particles of unknown type'
+        print*,j,' time = ',time(j)
+        gamma(j) = 1.666666666667
+        j = j + 1
 
-        enddo oversteps
+        !enddo oversteps
      endif
 
 55 continue
@@ -220,15 +228,24 @@ subroutine set_labels
   enddo
   label(ix(1:ndim)) = labelcoord(1:ndim,1)
   ivx = ndim+1
-  ipr = ndim+ndimV+2
-  label(ipr) = 'pressure'
-  ih = ndim+ndimV+3        !  smoothing length
-  label(ih) = 'h'
-  irho = ndim+ndimV+1     ! location of rho in data array
-  label(irho) = 'density'
-  iutherm = 0  !  thermal energy
-!  label(iutherm) = 'u'
-  ipmass = 8  !  particle mass
+
+  iamvec(5:6) = 5
+  labelvec(5:6) = 'f'
+  irho = 7
+  label(7) = 'density'
+  ipr = 8
+  label(8)  = 'pressure'
+  label(9)  = 'vorticity'
+  label(10) = 'pvisc'
+  label(11) = 'div v'
+  label(12) = 'hp'
+  ih = 12   !  smoothing length
+  label(13) = 'concentration'
+  label(14) = 'diffc'
+  label(15) = 'pmix'
+  iamvec(16:17) = 16
+  labelvec(16:17) = 'vhat'
+  ipmass = 18  !  particle mass
   label(ipmass) = 'particle mass'
 
   iamvec(ivx:ivx+ndimV-1) = ivx
@@ -239,14 +256,16 @@ subroutine set_labels
   !
   !--set labels for each particle type
   !
-  ntypes = 3 !!maxparttypes
+  ntypes = 4 !!maxparttypes
   labeltype(1) = 'fluid'
   labeltype(2) = 'boundary'
-  labeltype(3) = 'unknown'
+  labeltype(3) = 'other'
+  labeltype(4) = 'unknown'
 
   UseTypeInRenderings(1) = .true.
   UseTypeInRenderings(2) = .false.
-  UseTypeInRenderings(3) = .false.
+  UseTypeInRenderings(3) = .true.
+  UseTypeInRenderings(4) = .false.
 
 !-----------------------------------------------------------
 
