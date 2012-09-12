741bb9b9c589c119969984361e7527434535dcb3
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2012-09-12 03:48:29 +0000

    new kernel module added
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/splash@4769 cab04810-efc7-4a10-8ecf-f366c833a2ad

---
+++ src/kernels.f90
@@ -0,0 +1,168 @@
+!-----------------------------------------------------------------
+!
+!  This file is (or was) part of SPLASH, a visualisation tool 
+!  for Smoothed Particle Hydrodynamics written by Daniel Price:
+!
+!  http://users.monash.edu.au/~dprice/splash
+!
+!  SPLASH comes with ABSOLUTELY NO WARRANTY.
+!  This is free software; and you are welcome to redistribute
+!  it under the terms of the GNU General Public License
+!  (see LICENSE file for details) and the provision that
+!  this notice remains intact. If you modify this file, please
+!  note section 2a) of the GPLv2 states that:
+!
+!  a) You must cause the modified files to carry prominent notices
+!     stating that you changed the files and the date of any change.
+!
+!  Copyright (C) 2005-2012 Daniel Price. All rights reserved.
+!  Contact: daniel.price@monash.edu
+!
+!-----------------------------------------------------------------
+
+!-------------------------------------------------------------
+!
+!  Module containing kernel functions used for interpolation
+!
+!-------------------------------------------------------------
+module kernels
+ implicit none
+ integer, parameter, public :: nkernels = 2
+ character(len=17), dimension(0:nkernels), parameter, public :: kernelname = &
+    (/'default [cubic]  ', &
+      'M4 cubic spline  ', &
+      'M6 quintic spline'/)
+
+ real, parameter :: pi = 3.14159236
+ integer, public :: ikernel = 0
+ real, public  :: radkernel = 2.
+ real, public  :: radkernel2 = 4.
+ real, public  :: cnormk1D = 2./3.
+ real, public  :: cnormk2D = 10./(7.*pi)
+ real, public  :: cnormk3D = 1./pi
+ procedure(real), pointer :: wfunc
+ 
+ public :: wfunc, select_kernel, select_kernel_by_name
+ 
+ private
+
+contains
+
+!-----------------------------------------------
+!
+! Kernel selection routine, sets radkernel,
+! cnormk values and pointer to kernel function
+!
+!--------------------------------------
+subroutine select_kernel(j)
+ integer, intent(in) :: j
+
+ if (j.ge.1 .and. j.le.nkernels) then
+    print "(a,/)",' Using '//trim(kernelname(j))//' kernel' 
+ else
+    print "(a,/)",' Using default '//trim(kernelname(1))//' kernel'
+ endif
+
+ select case(j)
+ case(2) ! M6 quintic
+    ikernel = 2
+    radkernel = 3.0
+    cnormk1D = 1./120.
+    cnormk2D = 7./(478*pi)
+    cnormk3D = 1./(120.*pi)
+    wfunc => w_quintic
+ case default  !-- cubic spline kernel
+    if (j.eq.1) then
+       ikernel = 1 ! deliberately chose cubic spline
+    else
+       ikernel = 0 ! just whatever is the default
+    endif
+    radkernel = 2.0
+    cnormk1D = 2./3.
+    cnormk2D = 10./(7.*pi)
+    cnormk3D = 1./pi
+    wfunc => w_cubic
+ end select
+ radkernel2 = radkernel*radkernel
+
+end subroutine select_kernel
+
+!--------------------------------------
+!
+! Kernel selection based on string
+!
+!--------------------------------------
+subroutine select_kernel_by_name(string)
+ use asciiutils, only:lcase
+ character(len=*), intent(in) :: string
+ integer :: i,jkern
+ 
+ jkern = 0
+ !
+ !--check if string exactly matches a kernel name
+ !
+ do i=1,nkernels
+    if (trim(adjustl(lcase(string)))==trim(adjustl(lcase(kernelname(i))))) then
+       jkern = i
+    endif
+ enddo
+ !
+ !--if no match to a kernel name, look for other possible strings
+ !
+ if (ikernel.eq.0) then
+    select case(trim(adjustl(lcase(string))))
+    case('quintic','quintic spline','m6')
+       jkern = 2
+    case('cubic','cubic spline','m4')
+       jkern = 1
+    end select
+ endif
+
+ call select_kernel(jkern)
+
+end subroutine select_kernel_by_name
+
+!---------------------------------------
+!
+!  Functional forms of various kernels
+!
+!--------------------------------------
+pure elemental real function w_cubic(q2)
+ implicit none
+ real, intent(in) :: q2
+ real :: q
+
+ if (q2.lt.1.0) then
+    q = sqrt(q2)
+    w_cubic = 1.-1.5*q2 + 0.75*q2*q
+ elseif (q2.lt.4.0) then
+    q = sqrt(q2)
+    w_cubic = 0.25*(2.-q)**3
+ else
+    w_cubic = 0.
+ endif
+
+end function w_cubic
+
+pure elemental real function w_quintic(q2)
+ implicit none
+ real, intent(in) :: q2
+ real :: q,q4
+
+ if (q2.lt.1.0) then
+    q = sqrt(q2)
+    q4 = q2*q2
+    w_quintic = 66.-60.*q2 + 30.*q4 - 10.*q4*q
+ elseif ((q2.ge.1.0).and.(q2.lt.4.0)) then
+    q = sqrt(q2)
+    w_quintic = (3.-q)**5 - 6.*(2.-q)**5
+ elseif ((q2.ge.4.0).and.(q2.lt.9.0)) then
+    q = sqrt(q2)
+    w_quintic = (3.-q)**5
+ else
+    w_quintic = 0.0
+ endif
+
+end function w_quintic
+
+end module kernels
