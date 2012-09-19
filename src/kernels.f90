16f99dec1f6004a3a83e962d494c3f0f382ba762
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2012-09-19 23:55:32 +0000

    added kernel function for M5 quartic squashed to 2h
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/splash@4792 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/kernels.f90
+++ src/kernels.f90
@@ -27,11 +27,12 @@
 !-------------------------------------------------------------
 module kernels
  implicit none
- integer, parameter, public :: nkernels = 2
- character(len=17), dimension(0:nkernels), parameter, public :: kernelname = &
-    (/'default [cubic]  ', &
-      'M4 cubic spline  ', &
-      'M6 quintic spline'/)
+ integer, parameter, public :: nkernels = 3
+ character(len=22), dimension(0:nkernels), parameter, public :: kernelname = &
+    (/'default [cubic]       ', &
+      'M4 cubic spline   (2h)', &
+      'M5 quartic        (2h)', &
+      'M6 quintic spline (3h)'/)
 
  real, parameter :: pi = 3.14159236
  integer, public :: ikernel = 0
@@ -64,13 +65,20 @@ subroutine select_kernel(j)
  endif
 
  select case(j)
- case(2) ! M6 quintic
-    ikernel = 2
+ case(3) ! M6 quintic
+    ikernel = 3
     radkernel = 3.0
     cnormk1D = 1./120.
     cnormk2D = 7./(478*pi)
     cnormk3D = 1./(120.*pi)
     wfunc => w_quintic
+ case(2) ! M5 quintic, squashed to 2h
+    ikernel = 2
+    radkernel = 2.0
+    cnormk1D = 3125./24576.
+    cnormk2D = 46875./(153472.*pi)
+    cnormk3D = 15625./(65536.*pi)
+    wfunc => w_quartic2h
  case default  !-- cubic spline kernel
     if (j.eq.1) then
        ikernel = 1 ! deliberately chose cubic spline
@@ -112,6 +120,8 @@ subroutine select_kernel_by_name(string)
  if (ikernel.eq.0) then
     select case(trim(adjustl(lcase(string))))
     case('quintic','quintic spline','m6')
+       jkern = 3
+    case('quartic','quartic2h','m5','squashed quartic','quartic spline')
        jkern = 2
     case('cubic','cubic spline','m4')
        jkern = 1
@@ -164,5 +174,23 @@ pure elemental real function w_quintic(q2)
  endif
 
 end function w_quintic
+
+pure elemental real function w_quartic2h(q2)
+ implicit none
+ real, intent(in) :: q2
+ real :: q
+
+ q = sqrt(q2)
+ if (q.lt.0.4) then
+    w_quartic2h = (2.-q)**4 - 5.*(1.2-q)**4 + 10.*(0.4-q)**4
+ elseif (q.lt.1.2) then
+    w_quartic2h = (2.-q)**4 - 5.*(1.2-q)**4
+ elseif (q.lt.2.) then
+    w_quartic2h = (2.-q)**4
+ else
+    w_quartic2h = 0.
+ endif
+
+end function w_quartic2h
 
 end module kernels
