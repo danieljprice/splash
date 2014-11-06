1c65ccf0dd9aae64d034e910dc70709cc95f37d5
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2014-11-06 03:10:05 +0000

    higher quality movie options in movie.sh by default
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/splash@5194 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- scripts/movie.sh
+++ scripts/movie.sh
@@ -5,5 +5,5 @@
 #
 # DJP, Feb 2014
 #
-opts='-r 10 -b:v 2M -bt 5M -vcodec mpeg4 -vf setpts=4.*PTS'
+opts='-r 10 -vb 50M -bt 100M -vcodec mpeg4 -vf setpts=4.*PTS'
 ffmpeg -i splash_%04d.png $opts movie.mp4
