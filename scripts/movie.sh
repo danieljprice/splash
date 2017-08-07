b6022977ae3429d04810abd2a468f1d902df8a2f
Kieran Hirsh <kahir1@student.monash.edu>
2017-08-07 17:01:32 +1000

    movie.sh can now take infile_prefix and outfile_prefix as arguments, still works as before by default

--- scripts/movie.sh
+++ scripts/movie.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 #
-# script to make an mpeg4 movie from splash_*.png files
+# script to make an mpeg4 movie from png files
+# by default, takes splash_*.png as inputs and movie.mp4 as output
 # requires ffmpeg utility
 #
-# DJP, Feb 2014
+# DJP, Feb 2014. edited by KAH, Aug 2017
 #
-opts='-r 10 -vb 50M -bt 100M -vcodec mpeg4 -vf setpts=4.*PTS'
-ffmpeg -i splash_%04d.png $opts movie.mp4
+if [ $# -le 0 ]; then
+   opts='-r 10 -vb 50M -bt 100M -vcodec mpeg4 -vf setpts=4.*PTS'
+   ffmpeg -i splash_%04d.png $opts movie.mp4
+fi
+if [ $# -eq 1 ]; then
+   opts='-r 10 -vb 50M -bt 100M -vcodec mpeg4 -vf setpts=4.*PTS'
+   ffmpeg -i $1_%04d.png $opts movie.mp4
+fi
+if [ $# -eq 2 ]; then
+   opts='-r 10 -vb 50M -bt 100M -vcodec mpeg4 -vf setpts=4.*PTS'
+   ffmpeg -i $1_%04d.png $opts $2.mp4
+fi
+if [ $# -ge 3 ]; then
+   echo "usage: $0 infile_prefix outfile_prefix"
+   exit
+fi
