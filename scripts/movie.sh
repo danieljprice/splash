9455c9f1ab5ac400b18e51e810e9916c866c6642
Daniel Price <daniel.price@monash.edu>
2017-08-10 09:40:44 +1000

    cut unnecessary extra lines in movie.sh

--- scripts/movie.sh
+++ scripts/movie.sh
@@ -6,16 +6,14 @@
 #
 # DJP, Feb 2014. edited by KAH, Aug 2017
 #
+opts='-r 10 -vb 50M -bt 100M -vcodec mpeg4 -vf setpts=4.*PTS'
 if [ $# -le 0 ]; then
-   opts='-r 10 -vb 50M -bt 100M -vcodec mpeg4 -vf setpts=4.*PTS'
    ffmpeg -i splash_%04d.png $opts movie.mp4
 fi
 if [ $# -eq 1 ]; then
-   opts='-r 10 -vb 50M -bt 100M -vcodec mpeg4 -vf setpts=4.*PTS'
    ffmpeg -i $1_%04d.png $opts movie.mp4
 fi
 if [ $# -eq 2 ]; then
-   opts='-r 10 -vb 50M -bt 100M -vcodec mpeg4 -vf setpts=4.*PTS'
    ffmpeg -i $1_%04d.png $opts $2.mp4
 fi
 if [ $# -ge 3 ]; then
