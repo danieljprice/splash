3f7160e9e1c1f697193a66b45adeed3deb19b424
Daniel Price <daniel.price@monash.edu>
2021-01-29 18:08:51 +1100

    (lightcurve) preliminary lightcurve routine, compiles but not tested

--- src/lightcurve.f90
+++ src/lightcurve.f90
@@ -2,102 +2,108 @@ module lightcurve
  use params, only:int1,doub_prec
  implicit none
 
- ! public :: get_lightcurve
+ public :: get_lightcurve
  public :: get_temp_from_u
 
  private
 
 contains
 
-! subroutine get_lightcurve(time,ncolumns,dat,npartoftype,masstype,itype,ndim,ntypes,lum,rphoto,temp)
-!  use convert_grid, only:convert_to_grid
-!  use labels,       only:iutherm,ix
-!  use limits,       only:lim
-!  integer, intent(in)  :: ncolumns,ntypes,ndim
-!  integer, intent(in)  :: npartoftype(:)
-!  integer(kind=int1), intent(in) :: itype(:)
-!  real,    intent(in)  :: time
-!  real,    intent(in)  :: masstype(:)
-!  real,    intent(in)  :: dat(:,:)
-!  real,    intent(out) :: lum,rphoto,temp
-!  real, allocatable :: rhogrid(:,:,:),ugrid(:,:,:),tgrid(:,:,:)
-!  integer :: nlos,idim,nx,ny,nz
-!  real :: xmin(ndim),xmax(ndim)
-!
-!  if (ndim /= 3) then
-!     print "(a)",' ERROR: lightcurve only works with 3 dimensional data'
-!     return
-!  endif
-!  !
-!  ! interpolate SPH data to 3D grid to get density and thermal energy
-!  !
-!  call convert_to_grid(time,dat,ntypes,npartoftype,masstype,itype,ncolumns,'none',&
-!                       'none',.false.,icols=(/iutherm/),rhogrid=rhogrid,dat3D=ugrid)
-!  !
-!  ! convert thermal energy to temperature
-!  !
-!  print*,' max density = ',maxval(rhogrid)
-!  print*,' max utherm  = ',maxval(ugrid)
-!  tgrid = get_temp_from_u(rhogrid,ugrid)
-!  print*,' max temp    = ',maxval(tgrid)
-!  if (allocated(ugrid)) deallocate(ugrid)
-!  !
-!  ! find the photosphere and get the temperature and luminosity
-!  !
-!  nx = size(rhogrid,1)
-!  ny = size(rhogrid,2)
-!  nz = size(rhogrid,3)
-!  xmin(1:ndim) = lim(ix(1:ndim),1)
-!  xmax(1:ndim) = lim(ix(1:ndim),2)
-!  do idim=3,3
-!     call get_photosphere(idim,1,nx,ny,nz,rhogrid,tgrid,xmin,xmax,lum,rphoto,temp)
-!     !call get_photosphere(idim,-1,rhogrid,ugrid,lum,rphoto,temp)
-!  enddo
-!  !
-!  ! clean up memory
-!  !
-!  if (allocated(rhogrid)) deallocate(rhogrid)
-!  if (allocated(tgrid)) deallocate(tgrid)
-!
-! end subroutine get_lightcurve
+subroutine get_lightcurve(time,ncolumns,dat,npartoftype,masstype,itype,ndim,ntypes,lum,rphoto,temp)
+ use labels,                only:ix,ih,irho,ipmass,itemp,ikappa
+ use limits,                only:lim,get_particle_subset
+ use interpolate3D_opacity, only:interp3D_proj_opacity
+ use particle_data,         only:icolourme
+ use interpolation,         only:get_n_interp,set_interpolation_weights
+ use settings_data,         only:iRescale,iverbose,required,UseTypeInRenderings
+ use settings_part,         only:iplotpartoftype
+ use settings_render,       only:npix,inormalise=>inormalise_interpolations,&
+                                 idensityweightedinterpolation,exact_rendering
+ use settings_units,        only:units,unit_interp
+ integer, intent(in)  :: ncolumns,ntypes,ndim
+ integer, intent(in)  :: npartoftype(:)
+ integer(kind=int1), intent(in) :: itype(:)
+ real,    intent(in)  :: time
+ real,    intent(in)  :: masstype(:)
+ real,    intent(in)  :: dat(:,:)
+ real,    intent(out) :: lum,rphoto,temp
+ integer :: n,isinktype,npixx,npixy,ierr
+ real, dimension(3) :: xmin,xmax
+ real, dimension(:),   allocatable :: weight,x,y,z,flux,opacity
+ real, dimension(:,:), allocatable :: datpix,brightness
+ real :: zobs,dzobs,steboltz,dx,dy
 
-subroutine get_photosphere(idim,idir,nx,ny,nz,rho,temp,xmin,xmax,lum,rphoto,tphoto)
- integer, intent(in)  :: idim,idir,nx,ny,nz
- real,    intent(in)  :: rho(nx,ny,nz),temp(nx,ny,nz),xmin(:),xmax(:)
- real,    intent(out) :: lum,rphoto,tphoto
- real, allocatable :: tlast(:,:),tau(:,:)
- real :: dx,dy,dz,tau_crit,kappa
- integer :: k,j,i,ierr,npixels_in_photosphere
- real, parameter :: steboltz = 5.67051e-5
- real, parameter :: pi = 4.*atan(1.)
+ lum = 0.
+ rphoto = 0.
+ temp = 0.
+ if (ndim /= 3) then
+    print "(a)",' ERROR: lightcurve only works with 3 dimensional data'
+    return
+ endif
+ xmin(1:ndim) = lim(ix(1:ndim),1)
+ xmax(1:ndim) = lim(ix(1:ndim),2)
+ !
+ !--set number of particles to use in the interpolation routines
+ !  and allocate memory for weights
+ !
+ n = get_n_interp(ntypes,npartoftype,UseTypeInRenderings,iplotpartoftype,size(itype),.false.)
+ allocate(weight(n),x(n),y(n),z(n),flux(n),opacity(n),stat=ierr)
+ if (ierr /= 0) then
+    print*,' ERROR allocating memory for interpolation weights, aborting...'
+    return
+ endif
+ !
+ !--allocate memory for image
+ !
+ npixx = npix
+ dx = (xmax(1)-xmin(1))/npixx
+ npixy = int((xmax(2)-xmin(2) - 0.5*dx)/dx) + 1
+ dy = (xmax(2)-xmin(2))/npixy
+ print*,' using nx,ny,dx,dy = ',npixx,npixy,dx,dy
 
- tau_crit = 1.
- kappa = 0.2
+ allocate(datpix(npixx,npixy),brightness(npixx,npixy))
+ !
+ !--set interpolation weights (w = m/(rho*h^ndim)
+ !
+ isinktype = 0 !get_sink_type(ntypes)
+ call set_interpolation_weights(weight,dat,itype,(iplotpartoftype .and. UseTypeInRenderings),&
+      n,npartoftype,masstype,ntypes,ncolumns,irho,ipmass,ih,ndim,iRescale,&
+      idensityweightedinterpolation,inormalise,units,unit_interp,required,.false.,isinktype)
+ !
+ !--set default mask and apply range restrictions to data
+ !
+ icolourme(:) = 1
+ call get_particle_subset(icolourme,dat,ncolumns)
 
- select case(idim)
- case(3)
-    dz = (xmax(3) - xmin(3))/nz
-    dy = (xmax(2) - xmin(2))/ny
-    dx = (xmax(1) - xmin(1))/ny
-    allocate(tlast(nx,ny),tau(nx,ny),stat=ierr)
-    tau = 0.
-    tlast = 0.
-    do k=nz,1,-1
-       where(tau < tau_crit)
-          tlast(:,:) = temp(:,:,k)
-          tau(:,:)   = tau(:,:) + kappa*rho(:,:,k)*dz
-       end where
-    enddo
-    npixels_in_photosphere = count(tau > 1.)
-    lum = sum(steboltz*tlast**4*dx*dy,mask=(tau > 1.))
-    tphoto = sum(tlast,mask=(tau > 1.))/npixels_in_photosphere
-    print "(1x,g8.2,a)",100.*npixels_in_photosphere/real(nx*ny),'% of image is optically thick'
-    rphoto = sqrt(lum/(4.*pi*steboltz*tphoto**4))
-    print*,' tau (max,ave,min) = ',maxval(tau),sum(tau)/(nx*ny),minval(tau)
-    deallocate(tlast,tau)
- end select
+ !
+ ! specify opacity
+ !
+ if (ikappa > 0) then
+    opacity = dat(1:n,ikappa)
+ else
+    opacity = 0.3
+ endif
+ !
+ ! specify source function for each particle
+ !
+ steboltz = 5.67e-5 ! erg cm^-2 K^-4 s-1
+ flux = steboltz*dat(1:n,itemp)**4
+ !
+ ! raytrace SPH data to 2D image to get flux
+ !
+ zobs = huge(zobs)  ! no 3D perspective
+ dzobs = 0.
+ call interp3D_proj_opacity(x,y,z,&
+      dat(1:n,ipmass),n,dat(1:n,ih),weight, &
+      flux,z,icolourme(1:n), &
+      n,xmin(1),xmin(2),datpix,brightness,npixx,npixy,&
+      dx,dy,zobs,dzobs,opacity,huge(zobs),iverbose,exact_rendering)
 
-end subroutine get_photosphere
+ ! luminosity is integrated flux
+ lum = sum(datpix)*dx*dy
+ print*,'time = ',time,' luminosity = ',lum,' erg/s'
+
+end subroutine get_lightcurve
 
 real elemental function get_temp_from_u(rho,u) result(temp)
  real(doub_prec), intent(in) :: rho,u
