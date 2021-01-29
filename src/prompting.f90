75ae378d28bdbb96ac79b9a83ff03fffbeaf3212
Daniel Price <daniel.price@monash.edu>
2021-01-29 15:16:34 +1100

    (prompting) implicit none error fixed

--- src/prompting.f90
+++ src/prompting.f90
@@ -522,7 +522,7 @@ recursive subroutine string_prompt(text, string, length, case, noblank, list)
  integer, optional, intent(out)  :: length
  integer, optional, intent(in)   :: case
  logical, optional, intent(in)   :: noblank
- integer                         :: is, ia
+ integer                         :: is, ia, i
  integer, parameter              :: aoffset = 32
  logical                         :: allowblank,inlist
  character(len=*), dimension(:), intent(in), optional :: list
