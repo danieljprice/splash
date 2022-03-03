262ed4968b25efd707f232c7061bddbc0cc1edad
Daniel Price <daniel.price@monash.edu>
2022-03-04 08:12:41 +1100

    (globaldata) update tagline to 2022

--- src/globaldata.f90
+++ src/globaldata.f90
@@ -105,7 +105,7 @@ module filenames
  character(len=120) :: defaultsfile,limitsfile,unitsfile,coloursfile
  integer, dimension(maxfile) :: nstepsinfile
  character(len=*), parameter :: tagline = &
-  'SPLASH: A visualisation tool for SPH data (c)2004-2021 Daniel Price and contributors'
+  'SPLASH: A visualisation tool for SPH data (c)2004-2022 Daniel Price and contributors'
 
  public
 
